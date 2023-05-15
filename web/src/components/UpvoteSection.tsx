bf7fdff45cffb4646a94b2b1058f85ba04409a6c
Jasper Hale <halejasper1998@gmail.com>
2023-05-14 23:56:13 -0700

    changed ui component to IconButton  to get isloading prop working

--- web/src/components/UpvoteSection.tsx
+++ web/src/components/UpvoteSection.tsx
@@ -1,7 +1,7 @@
 import { ChevronUpIcon, ChevronDownIcon } from '@chakra-ui/icons';
-import { Flex, Box, Heading } from '@chakra-ui/react';
-import React from 'react'
-import { PostSnippetFragment, PostsQuery } from '../generated/graphql';
+import { Flex, Box, Heading, Skeleton, IconButton } from '@chakra-ui/react';
+import React, { useState } from 'react'
+import { PostSnippetFragment, PostsQuery, useVoteMutation } from '../generated/graphql';
 
 //can select types that you want
 // PostsQuery is just a big object
@@ -10,13 +10,43 @@ interface UpvoteSectionProps {
 }
 
 export const UpvoteSection: React.FC<UpvoteSectionProps> = ({post}) => {
-        return ( 
-        <Flex key={post.id} p={5} shadow='md' borderWidth='1px' >
+    const [loadingState, setLoadingState] = useState< "upvote-loading" | "downvote-loading" | "not-loading">("not-loading")
+
+    const [, vote] = useVoteMutation()     
+    return ( 
+        // <Flex key={post.id} p={5} shadow='md' borderWidth='1px' >
         <Flex direction="column" justifyContent="center" alignItems="center" mr={4}>
-        <ChevronUpIcon name="chevron-up" aria-label="upvote post" />
+    
+            <IconButton icon={<ChevronUpIcon/>} onClick={async ()=> {
+            setLoadingState("upvote-loading")
+            await vote({
+                postId: post.id,
+                value: 1
+            })
+            setLoadingState("not-loading")
+        }} 
+        
+        // 
+        name="chevron-up" 
+        aria-label="upvote post"
+        isLoading={loadingState === "upvote-loading"}
+        />
+
+    
+       
         {post.points}
-        <ChevronDownIcon name="chevron-down" onClick={()=>console.log("o")} aria-label="downvote post" />
+        <IconButton icon={<ChevronDownIcon/>} name="chevron-down" onClick={ async ()=>{
+            setLoadingState("downvote-loading")
+             vote({
+                postId: post.id,
+                value: -1
+            })
+            setLoadingState("not-loading")
+        }} aria-label="downvote post" 
+         isLoading={loadingState === "upvote-loading"}
+         />
         </Flex>
        
-    </Flex>);
+    // </Flex>
+    );
 }
